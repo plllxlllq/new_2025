6ed3c1d348dd9f5bbbc96511da29b944d7c75779
plllxlllq <plllxlllq@naver.com>
2025-09-19 13:55:16 +0900

    이미지추가

--- js/ui-script.js
+++ js/ui-script.js
@@ -87,7 +87,7 @@ const common = {
 /*** * krds_mainMenuPC * ***/
 const krds_mainMenuPC = {
   init() {
-    const gnbMenu = document.querySelector(".krds-main-menu:not(.sample) .gnb-menu");
+    const gnbMenu = document.querySelector(".main-menu:not(.sample) .gnb-menu");
 
     if (!gnbMenu) return;
 
@@ -188,11 +188,11 @@ const krds_mainMenuPC = {
     document.body.classList.toggle("hasScrollY", isEnabled && isScrollNeeded);
   },
   resetMainMenu() {
-    document.querySelectorAll(".krds-main-menu:not(.sample) .gnb-main-trigger:not(.is-link)").forEach((mainTrigger) => {
+    document.querySelectorAll(".main-menu:not(.sample) .gnb-main-trigger:not(.is-link)").forEach((mainTrigger) => {
       mainTrigger.classList.remove("active");
       mainTrigger.setAttribute("aria-expanded", "false");
     });
-    document.querySelectorAll(".krds-main-menu:not(.sample) .gnb-toggle-wrap").forEach((toggleWrap) => {
+    document.querySelectorAll(".main-menu:not(.sample) .gnb-toggle-wrap").forEach((toggleWrap) => {
       toggleWrap.classList.remove("is-open");
     });
   },
@@ -212,7 +212,7 @@ const krds_mainMenuPC = {
 
     // ESC 키를 눌러 메뉴를 닫거나, TAB 키로 초점이 메뉴 외부로 이동했을 때 메뉴 닫기
     document.addEventListener("keyup", (event) => {
-      if (event.code === "Escape" || !event.target.closest(".krds-main-menu")) {
+      if (event.code === "Escape" || !event.target.closest(".main-menu")) {
         this.closeMainMenu();
       }
     });
@@ -274,7 +274,7 @@ const krds_mainMenuPC = {
 /*** * krds_mainMenuMobile * ***/
 const krds_mainMenuMobile = {
   init() {
-    const mobileGnb = document.querySelector(".krds-main-menu-mobile:not(.sample)");
+    const mobileGnb = document.querySelector(".main-menu-mobile:not(.sample)");
 
     if (!mobileGnb) return;
 
@@ -379,7 +379,7 @@ const krds_mainMenuMobile = {
       mobileGnb.removeEventListener("transitionend", onTransitionEnd);
 
       // inert 설정
-      document.querySelector("#krds-header .header-in").setAttribute("inert", "");
+      document.querySelector("#header .header-in").setAttribute("inert", "");
       document.getElementById("container")?.setAttribute("inert", "");
       document.getElementById("footer")?.setAttribute("inert", "");
       
@@ -399,7 +399,7 @@ const krds_mainMenuMobile = {
     // header.style.zIndex = "";
 
     // inert 설정
-    document.querySelector("#krds-header .header-in").removeAttribute("inert");
+    document.querySelector("#header .header-in").removeAttribute("inert");
     document.getElementById("container")?.removeAttribute("inert");
     document.getElementById("footer")?.removeAttribute("inert");
     
@@ -558,144 +558,6 @@ const krds_mainMenuMobile = {
     document.querySelectorAll(".krds-main-menu-mobile .menu-wrap .gnb-main-trigger").forEach((menu) => {
       menu.classList.remove("active");
       menu.setAttribute("aria-selected", "false");
-    });
-  },
-};
-
-/*** * krds_sideNavigation * ***/
-const krds_sideNavigation = {
-  init() {
-    this.setupSideNavLists();
-    this.setupToggleEvents();
-    this.setupPopupEvents();
-  },
-  setupSideNavLists() {
-    const sideNavLists = document.querySelectorAll(".krds-side-navigation .lnb-list");
-    sideNavLists.forEach((navList) => {
-      const navItems = navList.querySelectorAll("li");
-      navItems.forEach((navItem) => this.setupNavItem(navItem));
-    });
-  },
-  setupNavItem(navItem) {
-    const navButton = navItem.querySelector(".lnb-btn");
-    if (!navButton || !navButton.className.includes("lnb-toggle")) return;
-
-    const uniqueIdx = `lnbmenu-${Math.random().toString(36).substring(2, 9)}`;
-    const navSubmenu = navButton.nextElementSibling;
-
-    // aria 설정
-    navButton.setAttribute("aria-controls", uniqueIdx);
-    navButton.setAttribute("aria-expanded", navButton.classList.contains("active"));
-
-    // 서브메뉴 id 설정 및 popup 처리
-    if (navButton.classList.contains("lnb-toggle-popup")) {
-      navButton.setAttribute("aria-haspopup", "true");
-    }
-    if (navSubmenu && navSubmenu.className.includes("lnb-submenu")) {
-      const navSubmenuList = navSubmenu.classList.contains("lnb-submenu-lv2") ? navSubmenu : navSubmenu.querySelector(":scope > ul");
-      navSubmenuList?.setAttribute("id", uniqueIdx);
-    }
-  },
-  setupToggleEvents() {
-    const toggleButtons = document.querySelectorAll(".krds-side-navigation .lnb-list:not(.exception-case) .lnb-toggle");
-    toggleButtons.forEach((toggleButton) => {
-      toggleButton.addEventListener("click", () => {
-        const expand = toggleButton.getAttribute("aria-expanded") !== "true";
-        
-        //this.toggleMenu(toggleButton, expand); 클릭 시 해당 버튼만 활성화 상태로 변경되어 해당 이벤트 주석처리.
-        //this.closeSiblingMenus(toggleButton);  클릭 시 해당 버튼만 활성화 상태로 변경되어 해당 이벤트 주석처리.
-        this.setActiveNav(toggleButton, expand);
-      });
-    });
-  },
-  setActiveNav(toggleButton, expand){
-    const parentListItem = toggleButton.closest("li");
-    toggleButton.setAttribute('aria-expanded', expand ? 'true' : 'false');
-    parentListItem.classList.toggle('active', expand);
-  },
-  setupPopupEvents() {
-    let lastClickedButton = null;
-
-    const popupToggleButtons = document.querySelectorAll(".lnb-toggle-popup");
-    const popupSubmenus = document.querySelectorAll(".lnb-submenu-lv2");
-
-    // 팝업 토글 버튼
-    popupToggleButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const popupSubmenu = button.nextElementSibling;
-        if (popupSubmenu && popupSubmenu.classList.contains("lnb-submenu-lv2")) {
-          popupSubmenu.classList.add("active");
-          button.setAttribute("aria-expanded", "true");
-
-          popupSubmenu.addEventListener(
-            "transitionend",
-            () => {
-              popupSubmenu.querySelector(".lnb-btn-tit")?.focus();
-            },
-            { once: true }
-          );
-
-          lastClickedButton = button;
-        }
-      });
-    });
-
-    // 서브 메뉴가 포커스를 잃으면 비활성화
-    popupSubmenus.forEach((popupSubmenu) => {
-      popupSubmenu.addEventListener("focusout", (event) => {
-        // 포커스가 서브메뉴 밖으로 나갔는지 확인
-        if (!popupSubmenu.contains(event.relatedTarget)) {
-          popupSubmenu.classList.remove("active");
-
-          if (lastClickedButton) {
-            lastClickedButton.setAttribute("aria-expanded", "false");
-            popupSubmenu.addEventListener(
-              "transitionend",
-              () => {
-                lastClickedButton.focus();
-              },
-              { once: true }
-            );
-          }
-        }
-      });
-
-      // lnb-btn-tit 클릭 시 서브메뉴 닫기
-      const subMenuTitleButton = popupSubmenu.querySelector(".lnb-btn-tit");
-      subMenuTitleButton?.addEventListener("click", () => {
-        lastClickedButton?.focus();
-      });
-    });
-  },
-  toggleMenu(toggleButton, expand) {
-    const parentListItem = toggleButton.closest("li");
-    toggleButton.setAttribute("aria-expanded", expand);
-    toggleButton.classList.toggle("active", expand);
-    parentListItem.classList.toggle("active", expand);
-  },
-  closeSiblingMenus(toggleButton) {
-    const parentListItem = toggleButton.closest("li");
-    const siblingButtons = parentListItem.parentNode.querySelectorAll(":scope > li > .lnb-toggle");
-    siblingButtons.forEach((siblingButton) => {
-      if (siblingButton !== toggleButton) {
-        this.toggleMenu(siblingButton, false); 
-      }
-    });
-  },
-  setActiveCurrentPage() {
-    // 활성화된 페이지를 찾는 예(개발 환경에 맞게 수정)
-    const currentPage = window.location.pathname.split("/").slice(-1)[0].replace(".html", "");
-    const lnbLinks = document.querySelectorAll(".krds-side-navigation .lnb-link");
-    lnbLinks.forEach((link) => {
-      const linkPage = link.getAttribute("href").split("/").pop().replace(".html", "");
-      if (linkPage === currentPage) {
-        link.closest(".lnb-item").classList.add("active");
-        link.closest(".lnb-item").querySelector(".lnb-toggle")?.classList.add("active");
-        link.closest(".lnb-item").querySelector(".lnb-toggle")?.setAttribute("aria-expanded", "true");
-        link.closest("li").classList.add("active");
-        // 접근성 현재 페이지 표시 aria-current
-        link.setAttribute("aria-current", "page");
-      }
     });
   },
 };
